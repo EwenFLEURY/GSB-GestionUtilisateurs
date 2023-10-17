753c5e5e78ac74525745ffc72c3744af9fa3e045
EwenFLEURY <ew.fleury@gmail.com>
2023-10-17 15:07:27 +0200

    1ere version interface graphique

---
+++ GSB_frais/src/gsb_frais/BaseDeDonnees.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gsb_frais;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author efleury
+ */
+public class BaseDeDonnees {
+    private Connection connexion;
+    private String url = "jdbc:mysql://localhost/gsb_frais";
+    private String user = "gsb";
+    private String passwd = "gsb";
+    public BaseDeDonnees(){
+        try{
+            this.connexion = DriverManager.getConnection(this.url, this.user, this.passwd);
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void ajouterUtilisateur(Utilisateur nouvelUtilisateur){
+        String sqlRequestValues = "'"+nouvelUtilisateur.getId()+"', '"+nouvelUtilisateur.getNom()+"', '"+nouvelUtilisateur.getPrenom()+"', '"+nouvelUtilisateur.getLogin()+"', '"+nouvelUtilisateur.getMdp()+"', '"+nouvelUtilisateur.getAdresse()+"', '"+nouvelUtilisateur.getCp()+"', '"+nouvelUtilisateur.getVille()+"', '"+nouvelUtilisateur.getDateEmbauche();
+        String sqlRequest = "INSERT INTO visiteur (id, nom, prenom, login, mdp, adresse, cp, ville, dateEmbauche) VALUES ("+sqlRequestValues+")";
+        try{
+            Statement stmt = connexion.createStatement();
+            stmt.executeQuery(sqlRequest);
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void supprimerUtilisateur(Utilisateur utilisateurASupprimer){
+        String sqlRequest = "DELETE FROM visiteur WHERE id="+utilisateurASupprimer.getId();
+        try{
+            Statement stmt = connexion.createStatement();
+            stmt.executeQuery(sqlRequest);
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean modifierUtilisateur(Utilisateur originel, Utilisateur nouvelUtilisateur){
+        String sqlRequestSet = "id="+nouvelUtilisateur.getId()+", nom="+nouvelUtilisateur.getNom()+", prenom="+nouvelUtilisateur.getPrenom()+", login="+nouvelUtilisateur.getLogin()+", mdp="+nouvelUtilisateur.getMdp()+", adresse="+nouvelUtilisateur.getAdresse()+", cp="+nouvelUtilisateur.getCp()+", ville="+nouvelUtilisateur.getVille()+", dateEmbauche="+nouvelUtilisateur.getDateEmbauche();
+        String sqlRequest = "UPDATE visiteur SET "+sqlRequestSet+" WHERE id="+originel.getId();
+        try{
+            Statement stmt = connexion.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlRequest);
+            if (rs.wasNull()){
+                return false;
+            }
+            return true;
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+    
+    public ArrayList<Utilisateur> getUtilisateurs(String chaineRecherche){
+        ArrayList<Utilisateur> users = new ArrayList<>();
+        String sqlRequest = "SELECT id, nom, prenom, login, mdp, adresse, cp, ville, dateEmbauche FROM visiteur";
+        try{
+            Statement stmt = connexion.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlRequest);
+            while (rs.next()) {
+                String userId = rs.getString("id");
+                String userNom = rs.getString("nom");
+                String userPrenom = rs.getString("prenom");
+                String userLogin = rs.getString("login");
+                String userMdp = rs.getString("mdp");
+                String userAdresse = rs.getString("adresse");
+                String userCp = rs.getString("cp");
+                String userVille = rs.getString("vile");
+                String userDateEmbauche = rs.getString("dateEmbauche");
+                if (userId.equals(chaineRecherche)){
+                    users.add(new Utilisateur(userId, userNom, userPrenom, userLogin, userMdp, userAdresse, userCp, userVille, userDateEmbauche));
+                }
+                else {
+                    if (userNom.equals(chaineRecherche)){
+                        users.add(new Utilisateur(userId, userNom, userPrenom, userLogin, userMdp, userAdresse, userCp, userVille, userDateEmbauche));
+                    }
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return users;
+    }
+    
+    public ArrayList<Utilisateur> getUtilisateurs(){
+        ArrayList<Utilisateur> users = new ArrayList<>();
+        String sqlRequest = "SELECT id, nom, prenom, login, mdp, adresse, cp, ville, dateEmbauche FROM visiteur";
+        try{
+            Statement stmt = connexion.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlRequest);
+            while (rs.next()) {
+                String userId = rs.getString("id");
+                String userNom = rs.getString("nom");
+                String userPrenom = rs.getString("prenom");
+                String userLogin = rs.getString("login");
+                String userMdp = rs.getString("mdp");
+                String userAdresse = rs.getString("adresse");
+                String userCp = rs.getString("cp");
+                String userVille = rs.getString("vile");
+                String userDateEmbauche = rs.getString("dateEmbauche");
+                users.add(new Utilisateur(userId, userNom, userPrenom, userLogin, userMdp, userAdresse, userCp, userVille, userDateEmbauche));
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return users;
+    }
+}
---
+++ GSB_frais/src/gsb_frais/Utilisateur.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gsb_frais;
+
+/**
+ *
+ * @author efleury
+ */
+public class Utilisateur {
+    private String id;
+    private String nom;
+    private String prenom;
+    private String login;
+    private String mdp;
+    private String adresse;
+    private String cp;
+    private String ville;
+    private String dateEmbauche;
+
+    public Utilisateur(String id, String nom, String prenom, String login, String mdp, String adresse, String cp, String ville, String dateEmbauche) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.login = login;
+        this.mdp = mdp;
+        this.adresse = adresse;
+        this.cp = cp;
+        this.ville = ville;
+        this.dateEmbauche = dateEmbauche;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public void setDateEmbauche(String dateEmbauche) {
+        this.dateEmbauche = dateEmbauche;
+    }    
+}
